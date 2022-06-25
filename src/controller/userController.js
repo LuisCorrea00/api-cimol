f319f7f68ab61f955656b91651a5a4f45fedabff
candidomv <candido@minhavisita.com.br>
2022-06-25 15:38:24 -0300

    Autenticação de usuário

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,21 @@
+const req = require("express/lib/request");
 const userModel=require("../model/userModel");
-exports.get= async ()=>{
-  return await userModel.get()
+
+exports.get= async (headers)=>{
+  auth=userModel.verifyJWT(headers['x-access-token']);
+  users={};
+  if(auth.idUser){
+    if(headers.iduser==auth.idUser){
+      resp=await userModel.get()
+    }else{ 
+      resp= {"status":"null", auth}
+    }
+  }else{
+    resp= {"status":"null", auth}
+  }
+  return resp;
+}
+
+exports.login= async (body)=>{
+  return await userModel.login(body)
 }
--- src/routers/route.js
+++ src/routers/route.js
@@ -7,7 +7,12 @@ router.get('/', (req, res, next)=>{
 })
  
 router.get('/user', async(req, res, next)=>{
-  user=await userController.get();
+  user=await userController.get(req.headers);
+  res.status(200).send(user);
+})
+
+router.post('/user/login', async(req, res, next)=>{
+  user=await userController.login(req.body);
   res.status(200).send(user);
 })
 
