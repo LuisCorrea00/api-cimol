6456f2d45dca8e04cf6f723b7a97d6ee54503a57
Cândido Farias <candido.cimol@gmail.com>
2022-07-07 11:03:50 -0300

    07/07/2022 - Ajustes na lógica de login

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -16,17 +16,38 @@ login= async (data)=>{
   " WHERE p.email='"+data.email+"' AND u.senha='"+data.senha+"' ";
  const usuarios = await  mysql.query(sql);
   result=null;
-  
-  if(usuarios[0].id){
-     const id=usuarios[0].id;
-     var token = jwt.sign({id} , 'CIMOL', {expiresIn:3600}); 
-     console.log("Fez login e gerou token!");
-     result={ auth: true, token: token , user:usuarios[0]}
+  if(usuarios){
+   if(usuarios.length>0){
+      if(usuarios[0].id){
+         const id=usuarios[0].id;
+         var token = jwt.sign({id} , 'CIMOL', {expiresIn:3600}); 
+         console.log("Fez login e gerou token!");
+          perfil="";
+          if(usuarios[0].aluno>0){
+             perfil+="aluno, ";
+          }
+          if(usuarios[0].professor>0){
+             perfil+="professor, ";
+          }
+          if(usuarios[0].admin>0){
+             perfil+="admin, ";
+          }
+          if(perfil.length>5){
+             perfil=perfil.substring(0, perfil.length-2)
+             sql="UPDATE usuario set perfil='"+perfil+"' WHERE pessoa_id_pessoa="+usuarios[0].id;
+             console.log(sql);
+             await  mysql.query(sql);
+    
+          }
+         result={ auth: true, token: token , user:usuarios[0],perfil}
+      }
+   }
   }
+  
   return result;
 }
 
-verifyJWT=(token)=>{ 
+verifyJWT= async (token, perfil)=>{ 
   if (!token){
      resp= { auth: false, message: 'Token não informado.' }; 
   }
@@ -34,12 +55,23 @@ verifyJWT=(token)=>{
   jwt.verify(token, 'CIMOL', function(err, decoded) { 
      
      if (err){
-        resp= { auth: false, message: 'Token inválido.' };
+        resp= { auth: false, message: 'Token inválido!' };
      }
      if(decoded){
-        resp= {auth:true, idUser:decoded.id};
+        
+      resp= {auth:true, idUser:decoded.id};
+      
+        
      }
   });
+  if(resp.auth){
+   sql="SELECT perfil FROM usuario WHERE pessoa_id_pessoa="+resp.idUser;
+   usuario= await  mysql.query(sql);
+   
+   if(usuario[0].perfil.indexOf(perfil)<0){
+       resp= { auth: false, message: 'Perfil Inválido!' };
+   }
+  }
   return resp;
 } 
 
