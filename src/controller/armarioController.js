fbfcb9bbff3397fc12f5ef03d24a36a558f05265
Gabrielly de Vargas <vargas.gabrielly16@gmail.com>
2022-09-06 10:50:02 -0300

    Atualizaçao armarios, arquivo faltante (armarioController)

--- src/controller/armarioController.js
+++ src/controller/armarioController.js
@@ -1,8 +1,6 @@
 const userModel=require("../model/userModel");
 const armariosModel=require("../model/armarioModel");
 
-
-
 exports.get= async (headers,idCurso)=>{
   auth=userModel.verifyJWT(headers['x-access-token'], headers['perfil']);
   if(auth.idUser){
@@ -17,3 +15,49 @@ exports.get= async (headers,idCurso)=>{
   console.log(resp);
   return resp;
 }
+
+exports.post = async (headers, body, idCurso)=>{
+  auth=userModel.verifyJWT(headers['x-acess-token']);
+  if(auth.idUser){
+    if(headers.iduser==auth.idUser){
+      resp=await armariosModel.post(body, idCurso);
+    }else{
+      resp={"status":"null", auth}
+    }
+  }else{
+    resp= {"status":"null", auth}
+  }
+  console.log(resp);
+  return resp;
+}
+
+exports.locar = async (_body, headers, idCurso)=>{
+  auth=serModel.verifyJWT(headers['x-acess-token']);
+  if(auth.idUser){
+    if(headers.iduser==auth.idUser){
+      resp=await armariosModel.locar(idCurso);
+    }else{
+      resp={"status":"null", auth}
+    }
+  }else{
+    resp={"status":"null", auth}
+  }
+  console.log(resp);
+  return resp;
+}
+
+/*exports.devolver=async(body, _headers, idCurso)=>{
+  auth=serModel.verifyJWT(body['x-acess-token']);
+  if(auth.idUser){
+    if(body.iduser==auth.idUser){
+      resp=await armariosModel.devolver(idCurso);;
+    }else{
+      resp={"status":"null", auth}
+    }
+  }else{
+    resp={"status":"null", auth}
+  }
+  console.log(resp);
+  return resp;
+}*/
+
